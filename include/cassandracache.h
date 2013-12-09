50d885cf5ecf053c3f7f08564010c9a580134e3e
Alex Hockey <alex.hockey@metaswitch.com>
2013-12-09 19:34:45 +0000

    Refactor to use classes on the cache interface rather than synchronous function calls

--- include/cassandracache.h
+++ include/cassandracache.h
@@ -48,22 +48,7 @@ using namespace org::apache::cassandra;
 class CassandraCache
 {
 public:
-  //
-  // Exceptions that may be thown by this class.
-  //
-  class Exception
-  {
-  public:
-    inline Exception(const char* func, int rc) : _func(func), _rc(rc) {};
-    const char* _func;
-    const int _rc;
-  };
-
-  // A entry could not be found in the cache.
-  class NotFound : Exception {};
-
-  // There was an error communicating with Cassandra.
-  class CassandraError : Exception {};
+  virtual ~CassandraCache();
 
   //
   // Methods to manage the cache instance. These mirror the methods used to
@@ -78,52 +63,247 @@ public:
   void stop();
   void wait_stopped();
 
-  //
-  // Methods to set, get and delete items in the cache.
-  //
-  // Methods that moidy the cache must take a timestamp parameter. Related
-  // updates should be made with the same timestamp, which ensures the cache
-  // ends up being (eventually) consistent.
-  //
-  // The 'put' methods also take a time to live (TTL) parameter. This specifys
-  // how long the entry exists in the cache before being automatically expired.
-  // 0 => the entry never expires. Modifying the row resets the expiry time.
-  //
-  void put_imssubscription(std::string& public_id,
-                           std::string& xml,
-                           int64_t timestamp,
-                           int32_t ttl = 0);
-  void put_multi_imssubscription(std::vector<std::string>& public_ids,
-                                 std::string& xml,
-                                 int64_t timestamp,
-                                 int32_t ttl = 0);
-  std::string* get_imssubscription(std::string& public_id);
+  enum Error
+  {
+    NONE = 0
+  };
 
-  void put_assoc_public_id(std::string& private_id,
-                           std::string& assoc_public_id,
-                           int64_t timestamp,
-                           int32_t ttl = 0);
-  std::vector<std::string>* get_assoc_public_ids(std::string& private_id);
+  // Class representing a cassandra request.
+  class Request
+  {
+  public:
+    Request(std::string& table);
+    virtual ~Request();
 
-  void put_auth_vector(std::string& private_id,
-                       DigestAuthVector& auth_vector,
+    virtual Error send(CassandraCache *cache);
+
+  private:
+    virtual void on_success() {};
+
+    virtual void on_error(Error error_code)
+    {
+      // TODO write a debug log.
+    };
+
+    std::string _table;
+  };
+
+  // Class representing a request to modify the cassandra cache - for example
+  // putting some columns, deleting rows, etc.
+  class ModificationRequest : Request
+  {
+  public:
+    ModificationRequest(std::string& table, int64_t timestamp);
+    virtual ~ModificationRequest();
+
+  private:
+    int64_t _timestamp;
+  };
+
+  // Class rerpresenting a request to put some data into the cassandra cache.
+  class PutRequest : ModificationRequest
+  {
+  public:
+    PutRequest(std::string& table, int64_t timestamp, int32_t ttl = 0);
+    virtual ~PutRequest();
+
+  private:
+    int32_t _ttl;
+
+    void _put_columns(std::vector<std::string>& keys,
+                      std::map<std::string, std::string>& columns,
+                      int64_t timestamp,
+                      int32_t ttl);
+
+  };
+
+  // Class representing a request to get some data from the cassandra cache.
+  class GetRequest : Request
+  {
+  public:
+    virtual ~GetRequest();
+
+  private:
+    void _ha_get_row(std::string& key,
+                     std::vector<ColumnOrSuperColumn>& columns);
+
+    void _ha_get_columns(std::string& key,
+                         std::vector<std::string>& names,
+                         std::vector<ColumnOrSuperColumn>& columns);
+
+    void _ha_get_columns_with_prefix(std::string& key,
+                                     std::string& prefix,
+                                     std::vector<ColumnOrSuperColumn>& columns);
+
+    void _get_row(std::string& key,
+                  std::vector<ColumnOrSuperColumn>& columns,
+                  ConsistencyLevel consistency_level);
+
+    void _get_columns(std::string& key,
+                      std::vector<std::string>& names,
+                      std::vector<ColumnOrSuperColumn>& columns,
+                      ConsistencyLevel consistency_level);
+
+    void _get_columns_with_prefix(std::string& key,
+                                  std::string& prefix,
+                                  std::vector<ColumnOrSuperColumn>& columns,
+                                  ConsistencyLevel consistency_level);
+
+    void _issue_get_for_key(std::string& key,
+                            SlicePredicate& predicate,
+                            std::vector<ColumnOrSuperColumn>& columns);
+  };
+
+  // Class representing a request to delete some rows from the cassandra cache.
+  class DeleteRowsRequest : ModificationRequest
+  {
+  public:
+    DeleteRowsRequest(std::string& table,
+                      std::vector<std::string>& keys,
+                      int64_t timestamp);
+    virtual ~DeleteRowsRequest();
+
+  private:
+    void _delete_row(std::string& key,
+                     int64_t timestamp);
+  };
+
+  // A request to put an IMS subscription XML document into the cache.
+  class PutIMSSubscription : PutRequest
+  {
+  public:
+    PutIMSSubscription(std::string& public_id,
+                       std::string& xml,
                        int64_t timestamp,
                        int32_t ttl = 0);
-  DigestAuthVector *get_auth_vector(std::string& private_id,
-                                    std::string* public_id = NULL);
+    PutIMSSubscription(std::vector<std::string>& public_ids,
+                       std::string& xml,
+                       int64_t timestamp,
+                       int32_t ttl = 0);
+    virtual ~PutIMSSubscription();
 
-  void delete_public_id(std::string& public_id, int64_t timestamp);
-  void delete_multi_public_id(std::vector<std::string>& public_ids,
-                              int64_t timestamp);
-  void delete_private_id(std::string& private_id, int64_t timestamp);
-  void delete_multi_private_id(std::vector<std::string>& private_ids,
-                               int64_t timestamp);
+    Error send(CassandraCache *cache);
+
+  private:
+    std::vector<std::string> _public_ids;
+    std::string _xml;
+  };
+
+  // A request to associate a public ID with a particular private ID.
+  class PutAssociatedPublicID : PutRequest
+  {
+  public:
+    PutAssociatedPublicID(std::string& private_id,
+                          std::string& assoc_public_id,
+                          int64_t timestamp,
+                          int32_t ttl = 0);
+    virtual ~PutAssociatedPublicID();
+
+    Error send(CassandraCache *cache);
+
+  private:
+    std::string& _private_id;
+    std::string _assoc_public_id;
+  };
+
+  // A request to add an authorization vector to the cache.
+  class PutAuthVector : PutRequest
+  {
+  public:
+    PutAuthVector(std::string& private_id,
+                  DigestAuthVector& auth_vector,
+                  int64_t timestamp,
+                  int32_t ttl = 0);
+    virtual ~PutAuthVector();
+
+    Error send(CassandraCache *cache);
+
+  private:
+    std::string _private_id;
+    DigestAuthVector *auth_vector;
+  };
+
+  // A request to get the IMS subscription XML for a public ID.
+  class GetIMSSubscription : GetRequest
+  {
+  public:
+    GetIMSSubscription(std::string& public_id);
+    virtual ~GetIMSSubscription();
+
+    Error send(CassandraCache *cache);
+
+  private:
+    std::string _public_id;
+
+    void on_success(std::string& xml) {};
+  };
+
+  // A request to get the public IDs associated with a private ID.
+  class GetAssociatedPublicIDs : GetRequest
+  {
+    GetAssociatedPublicIDs(std::string& private_id);
+    virtual ~GetAssociatedPublicIDs();
+
+    Error send(CassandraCache *cache);
+
+  private:
+    std::string _private_id;
+
+    void on_success(std::vector<std::string>& public_ids) {};
+  };
+
+  // A request to get the authorization vector for a private ID.
+  class GetAuthVector : GetRequest
+  {
+  public:
+    GetAuthVector(std::string& private_id);
+    GetAuthVector(std::string& private_id,
+                  std::string& public_id);
+    virtual ~GetAuthVector();
+
+    Error send(CassandraCache *cache);
+
+  private:
+    std::string _private_id;
+    std::string _public_id;
+
+    void on_success(DigestAuthVector *auth_vector);
+  };
+
+  // A request to delete one or more public IDs.
+  class DeletePublicIDs : DeleteRowsRequest
+  {
+  public:
+    DeletePublicIDs(std::string& public_id, int64_t timestamp);
+    DeletePublicIDs(std::vector<std::string>& public_ids,
+                    int64_t timestamp);
+    virtual ~DeletePublicIDs();
+
+    Error send(CassandraCache *cache);
+
+  private:
+    std::vector<std::string> public_ids;
+  };
+
+  // A request to delete one or more private IDs.
+  class DeletePrivateIDs : DeleteRowsRequest
+  {
+  public:
+    DeletePrivateIDs(std::string& public_id, int64_t timestamp);
+    DeletePrivateIDs(std::vector<std::string>& public_ids,
+                     int64_t timestamp);
+    virtual ~DeletePrivateIDs();
+
+    Error send(CassandraCache *cache);
+
+  private:
+    std::vector<std::string> public_ids;
+  };
 
   // Return the current time (in micro-seconds). This timestamp is suitable to
   // use with methods that modify the cache.
   int64_t generate_timestamp(void);
 
-  virtual ~CassandraCache();
 private:
 
   // Singleton variables.
@@ -133,57 +313,21 @@ private:
   // The keyspace the cache is stored in.
   static const std::string KEYSPACE;
 
+
   // Cassandra connection details.
   std::string _cass_host;
   uint16_t _cass_port;
+
+  pthread_key_t _thread_local;
+
+  // Get a thread-specific Cassandra connection.
+  CassandraClient* _get_client();
 
   // The constructors and assignment operation are private to prevent multiple
   // instances of the class from being created.
   CassandraCache();
   CassandraCache(CassandraCache const &);
   void operator=(CassandraCache const &);
-
-  // Get a thread-specific Cassandra connection.
-  CassandraClient* _get_client();
-
-  //
-  // Utility methods to manipulate cassandra rows/columns.
-  //
-  void _modify_column(std::string& key,
-                      std::string& name,
-                      std::string& val,
-                      int64_t timestamp,
-                      int32_t ttl);
-
-  void _get_row(std::string& key,
-                std::vector<ColumnOrSuperColumn>& columns,
-                ConsistencyLevel consistency_level);
-
-  void _ha_get_row(std::string& key,
-                   std::vector<ColumnOrSuperColumn>& columns);
-
-  void _get_columns(std::string& key,
-                    std::vector<std::string>& names,
-                    std::vector<ColumnOrSuperColumn>& columns,
-                    ConsistencyLevel consistency_level);
-  void _ha_get_columns(std::string& key,
-                       std::vector<std::string>& names,
-                       std::vector<ColumnOrSuperColumn>& columns);
-
-  void _get_columns_with_prefix(std::string& key,
-                                std::string& prefix,
-                                std::vector<ColumnOrSuperColumn>& columns,
-                                ConsistencyLevel consistency_level);
-  void _ha_get_columns_with_prefix(std::string& key,
-                                   std::string& prefix,
-                                   std::vector<ColumnOrSuperColumn>& columns);
-
-  void _delete_row(std::string& key);
-
-  void _serialize_digest_auth_vector(DigestAuthVector& auth_vector,
-                                     std::vector<Mutation>& columns);
-  void _deserialize_digest_auth_vector(std::vector<ColumnOrSuperColumn>& columns,
-                                       DigestAuthVector& auth_vector);
 };
 
 #endif
--- src/cassandracache.cpp
+++ src/cassandracache.cpp
@@ -35,3 +35,246 @@
  */
 
 #include <cassandracache.h>
+
+void CassandraCache::initialize()
+{
+  // No-op
+}
+
+void CassandraCache::configure(std::string cass_hostname,
+                               uint16_t cass_port)
+{
+  _cass_host = cass_hostname;
+  _cass_port = cass_port;
+}
+
+void CassandraCache::start()
+{
+  // Check connectivity to cassandra. Throw if there's a problem.
+}
+
+void CassandraCache::stop()
+{
+  // No-op
+}
+
+void CassandraCache::wait_stopped()
+{
+  // No-op
+}
+
+CassandraClient* CassandraCache::_get_client()
+{
+  // Get client out of thread-local data.
+  // If not found create one and store in thread local data.
+  // Return client.
+  return NULL;
+}
+
+
+
+
+
+
+void CassandraCache::PutRequest::
+_put_columns(std::vector<std::string>& keys,
+             std::map<std::string, std::string>& columns,
+             int64_t timestamp,
+             int32_t ttl)
+{
+  // Create a mutation for the column.
+  // Create a vector of mutations (m).
+  // Create a mutation map (key1 => m, key2 => m, ...)
+  // Call _get_client()->batch_mutate().
+  //
+  // Catch thrift exceptions and convert them into on_error calls.
+}
+
+
+
+
+
+
+
+
+void CassandraCache::GetRequest::
+_ha_get_row(std::string& key,
+            std::vector<ColumnOrSuperColumn>& columns)
+{
+  // Call _get_row with a consistency level of ONE.
+  // If this throws an NotFound exception, retry with QUORUM.
+}
+
+void CassandraCache::GetRequest::
+_ha_get_columns(std::string& key,
+                std::vector<std::string>& names,
+                std::vector<ColumnOrSuperColumn>& columns)
+{
+  // Call _get_columns with a consistency level of ONE.
+  // If this throws an NotFound exception, retry with QUORUM.
+}
+
+void CassandraCache::GetRequest::
+_ha_get_columns_with_prefix(std::string& key,
+                            std::string& prefix,
+                            std::vector<ColumnOrSuperColumn>& columns)
+{
+  // Call _get_columns_with_prefix with a consistency level of ONE.
+  // If this throws an NotFound exception, retry with QUORUM.
+}
+
+void CassandraCache::GetRequest::
+_get_row(std::string& key,
+         std::vector<ColumnOrSuperColumn>& columns,
+         ConsistencyLevel consistency_level)
+{
+  // Create a slice prediciate that gets all columns.
+  // _issue_get_for_key()
+}
+
+void CassandraCache::GetRequest::
+_get_columns(std::string& key,
+             std::vector<std::string>& names,
+             std::vector<ColumnOrSuperColumn>& columns,
+             ConsistencyLevel consistency_level)
+{
+  // Create a slice prediciate that gets the specified columns.
+  // _issue_get_for_key()
+}
+
+void CassandraCache::GetRequest::
+_get_columns_with_prefix(std::string& key,
+                         std::string& prefix,
+                         std::vector<ColumnOrSuperColumn>& columns,
+                         ConsistencyLevel consistency_level)
+{
+  // Create a slice prediciate that gets columns that have the specified prefix.
+  // Fill in the `columns` vector with the results.
+  // Strip the prefix off the resulting columns.
+}
+
+void CassandraCache::GetRequest::
+_issue_get_for_key(std::string& key,
+                   SlicePredicate& predicate,
+                   std::vector<ColumnOrSuperColumn>& columns)
+{
+  // Create a keyrange specifying the key.
+  // Call _get_client()->get_range_slices()
+  // Output the resulting columns vector.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+void CassandraCache::DeleteRowsRequest::
+_delete_row(std::string& key,
+            int64_t timestamp)
+{
+  // Create a ColumnPath specifying all columns.
+  // Call _get_client->remove()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CassandraCache::Error CassandraCache::PutIMSSubscription::
+send(CassandraCache *cache)
+{
+  // _put_columns()
+  // Catch thrift exceptions and convert to error codes.
+  // Call on_success or on_falure as appropriate.
+  return Error::NONE;
+}
+
+
+CassandraCache::Error CassandraCache::PutAssociatedPublicID::
+send(CassandraCache *cache)
+{
+  // Add a assoc_public_ prefix to the public ID.
+  // _put_columns()
+  // Catch thrift exceptions and convert to error codes.
+  // Call on_success or on_falure as appropriate.
+  return Error::NONE;
+}
+
+
+CassandraCache::Error CassandraCache::PutAuthVector::
+send(CassandraCache *cache)
+{
+  // Convert the DigestAuthVector to a map of columns names => values.
+  // _put_columns()
+  // Catch thrift exceptions and convert to error codes.
+  // Call on_success or on_falure as appropriate.
+  return Error::NONE;
+}
+
+
+CassandraCache::Error CassandraCache::GetIMSSubscription::
+send(CassandraCache *cache)
+{
+  // _ha_get_column()
+  // Catch thrift exceptions and convert to error codes.
+  // Call on_success or on_falure as appropriate.
+  return Error::NONE;
+}
+
+
+CassandraCache::Error CassandraCache::GetAssociatedPublicIDs::
+send(CassandraCache *cache)
+{
+  // _ha_get_columns_with_prefix()
+  // Catch thrift exceptions and convert to error codes.
+  // Call on_success or on_falure as appropriate.
+  return Error::NONE;
+}
+
+
+CassandraCache::Error CassandraCache::GetAuthVector::
+send(CassandraCache *cache)
+{
+  // _ha_get_columns()
+  // Construct a DigestAuthVector from the values returned (error if some are
+  // missing).
+  // Catch thrift exceptions and convert to error codes.
+  // Call on_success or on_falure as appropriate.
+  return Error::NONE;
+}
+
+CassandraCache::Error CassandraCache::DeletePublicIDs::
+send(CassandraCache *cache)
+{
+  // _delete_row()
+  // Catch thrift exceptions and convert to error codes.
+  // Call on_success or on_falure as appropriate.
+  return Error::NONE;
+}
+
+CassandraCache::Error CassandraCache::DeletePrivateIDs::
+send(CassandraCache *cache)
+{
+  // _delete_row()
+  // Catch thrift exceptions and convert to error codes.
+  // Call on_success or on_falure as appropriate.
+  return Error::NONE;
+}
