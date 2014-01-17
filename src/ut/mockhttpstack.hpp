d1ebf370d1b0871b5fa7a4d38d34bbc976bf36a3
Matt Williams <matt.williams@metaswitch.com>
2014-01-17 20:02:42 +0000

    HttpStack Mock and basic handler test

--- src/ut/mockhttpstack.hpp
+++ src/ut/mockhttpstack.hpp
@@ -43,12 +43,46 @@
 class MockHttpStack : public HttpStack
 {
 public:
+  class Request : public HttpStack::Request
+  {
+  public:
+    Request(HttpStack* stack, std::string path, std::string file, std::string query = "") : HttpStack::Request(stack, evhtp_request(path, file, query)) {}
+    ~Request()
+    {
+      evhtp_connection_t* conn = _req->conn;
+      evhtp_request_free(_req);
+      free(conn);
+    }
+    std::string content()
+    {
+      size_t len = evbuffer_get_length(_req->buffer_out);
+      return std::string((char*)evbuffer_pullup(_req->buffer_out, len), len);
+    }
+
+  private:
+    static evhtp_request_t* evhtp_request(std::string path, std::string file, std::string query = "")
+    {
+      evhtp_request_t* req = evhtp_request_new(NULL, NULL);
+      req->conn = (evhtp_connection_t*)calloc(sizeof(evhtp_connection_t), 1);
+      req->uri = (evhtp_uri_t*)calloc(sizeof(evhtp_uri_t), 1);
+      req->uri->path = (evhtp_path_t*)calloc(sizeof(evhtp_path_t), 1);
+      req->uri->path->full = strdup((path + file).c_str());
+      req->uri->path->file = strdup(file.c_str());
+      req->uri->path->path = strdup(path.c_str());
+      req->uri->path->match_start = (char*)calloc(1, 1);
+      req->uri->path->match_end = (char*)calloc(1, 1);
+      req->uri->query = evhtp_parse_query(query.c_str(), query.length());
+      return req;
+    }
+  };
+
   MOCK_METHOD0(initialize, void());
   MOCK_METHOD4(configure, void(const std::string&, unsigned short, int, AccessLogger*));
   MOCK_METHOD2(register_handler, void(char*, BaseHandlerFactory*));
   MOCK_METHOD0(start, void());
   MOCK_METHOD0(stop, void());
   MOCK_METHOD0(wait_stopped, void());
+  MOCK_METHOD2(send_reply, void(HttpStack::Request&, int));
 };
 
 #endif
