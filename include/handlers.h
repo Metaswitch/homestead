0920a6809496168f4f8d3e3ac43908cab486fc50
Graeme Robertson <graeme.robertson@metaswitch.com>
2014-01-29 10:36:29 +0000

    Final mark ups for PPR and RTR (and up to date cpp-common)

--- include/handlers.h
+++ include/handlers.h
@@ -339,7 +339,13 @@ class RegistrationTerminationHandler : public Diameter::Stack::Handler
 public:
   struct Config
   {
-    Config(Cache* _cache, Cx::Dictionary* _dict, int _ims_sub_cache_ttl = 3600) : cache(_cache), dict(_dict), ims_sub_cache_ttl(_ims_sub_cache_ttl) {}
+    Config(Cache* _cache,
+           Cx::Dictionary* _dict,
+           int _ims_sub_cache_ttl = 3600) :
+           cache(_cache),
+           dict(_dict),
+           ims_sub_cache_ttl(_ims_sub_cache_ttl) {}
+
     Cache* cache;
     Cx::Dictionary* dict;
     int ims_sub_cache_ttl;
@@ -368,7 +374,13 @@ class PushProfileHandler : public Diameter::Stack::Handler
 public:
   struct Config
   {
-    Config(Cache* _cache, Cx::Dictionary* _dict, int _impu_cache_ttl = 0, int _ims_sub_cache_ttl = 3600) : cache(_cache), dict(_dict), ims_sub_cache_ttl(_ims_sub_cache_ttl) {}
+    Config(Cache* _cache, Cx::Dictionary* _dict,
+           int _impu_cache_ttl = 0,
+           int _ims_sub_cache_ttl = 3600) :
+           cache(_cache),
+           dict(_dict),
+           ims_sub_cache_ttl(_ims_sub_cache_ttl) {}
+
     Cache* cache;
     Cx::Dictionary* dict;
     int impu_cache_ttl;
