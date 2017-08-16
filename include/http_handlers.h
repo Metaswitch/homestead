12763e722318c4adef55e480a07262b922a8e7a0
Sebastian Rex <sebastian.rex@metaswitch.com>
2017-08-16 10:55:00 +0100

    Remove unused config options

--- include/http_handlers.h
+++ include/http_handlers.h
@@ -173,12 +173,7 @@ class ImpuLocationInfoTask : public HssCacheTask
 public:
   struct Config
   {
-    Config(bool _hss_configured = true,
-           int _diameter_timeout_ms = 200) :
-      hss_configured(_hss_configured),
-      diameter_timeout_ms(_diameter_timeout_ms) {}
-    bool hss_configured;
-    int diameter_timeout_ms;
+    Config() {}
   };
 
   ImpuLocationInfoTask(HttpStack::Request& req, const Config* cfg, SAS::TrailId trail) :
@@ -203,18 +198,15 @@ public:
     Config(bool _hss_configured = true,
            int _hss_reregistration_time = 3600,
            int _record_ttl = 7200,
-           int _diameter_timeout_ms = 200,
            bool _support_shared_ifcs = true) :
       hss_configured(_hss_configured),
       hss_reregistration_time(_hss_reregistration_time),
       record_ttl(_record_ttl),
-      diameter_timeout_ms(_diameter_timeout_ms),
       support_shared_ifcs(_support_shared_ifcs) {}
 
     bool hss_configured;
     int hss_reregistration_time;
     int record_ttl;
-    int diameter_timeout_ms;
     bool support_shared_ifcs;
   };
 
