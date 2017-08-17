7c041a77f59fc0ffcfa9237895feb5c60d383943
Richard Whitehouse <richard.whitehouse@metaswitch.com>
2017-08-17 13:21:24 +0000

    More IMPU Store UTs

--- src/ut/impu_store_test.cpp
+++ src/ut/impu_store_test.cpp
@@ -15,6 +15,7 @@
 #include "test_utils.hpp"
 
 static const std::string IMPU = "sip:impu@example.com";
+static const std::string ASSOC_IMPU = "sip:assoc_impu@example.com";
 static const std::vector<std::string> NO_ASSOCIATED_IMPUS;
 static const ChargingAddresses NO_CHARGING_ADDRESSES = ChargingAddresses({}, {});
 static const std::vector<std::string> IMPI = { "impi@example.com" };
@@ -101,15 +102,15 @@ TEST_F(ImpuStoreTest, GetDefaultImpu)
 
   delete default_impu;
 
-  ImpuStore::Impu* impu = impu_store->get_impu(IMPU.c_str(), 0L);
+  ImpuStore::Impu* got_impu = impu_store->get_impu(IMPU.c_str(), 0L);
 
-  ASSERT_NE(nullptr, impu);
-
-  ASSERT_TRUE(impu->is_default_impu());
-  ASSERT_EQ(now, impu->expiry);
+  ASSERT_NE(nullptr, got_impu);
+  ASSERT_EQ(IMPU, got_impu->impu);
+  ASSERT_TRUE(got_impu->is_default_impu());
+  ASSERT_EQ(now, got_impu->expiry);
 
   ImpuStore::DefaultImpu* got_default_impu =
-    dynamic_cast<ImpuStore::DefaultImpu*>(impu);
+    dynamic_cast<ImpuStore::DefaultImpu*>(got_impu);
 
   ASSERT_NE(nullptr, got_default_impu);
 
@@ -122,4 +123,105 @@ TEST_F(ImpuStoreTest, GetDefaultImpu)
   delete got_default_impu;
   delete impu_store;
   delete local_store;
+}
+
+TEST_F(ImpuStoreTest, SetAssociatedImpu)
+{
+  LocalStore* local_store = new LocalStore();
+  ImpuStore* impu_store = new ImpuStore(local_store);
+
+  int now = time(0);
+
+  ImpuStore::AssociatedImpu* assoc_impu =
+    new ImpuStore::AssociatedImpu(ASSOC_IMPU,
+                                  IMPU,
+                                  0L,
+                                  now);
+
+  impu_store->set_impu(assoc_impu, 0);
+
+  delete assoc_impu;
+  delete impu_store;
+  delete local_store;
+}
+
+TEST_F(ImpuStoreTest, GetAssociatedImpu)
+{
+  LocalStore* local_store = new LocalStore();
+  ImpuStore* impu_store = new ImpuStore(local_store);
+
+  int now = time(0);
+
+  ImpuStore::AssociatedImpu* assoc_impu =
+    new ImpuStore::AssociatedImpu(ASSOC_IMPU,
+                                  IMPU,
+                                  0L,
+                                  now);
+
+  impu_store->set_impu(assoc_impu, 0);
+
+  delete assoc_impu;
+
+  ImpuStore::Impu* got_impu = impu_store->get_impu(ASSOC_IMPU, 0);
+
+  ASSERT_NE(nullptr, got_impu);
+  ASSERT_EQ(ASSOC_IMPU, got_impu->impu);
+  ASSERT_FALSE(got_impu->is_default_impu());
+  ASSERT_EQ(now, got_impu->expiry);
+
+  ImpuStore::AssociatedImpu* got_associated_impu =
+    dynamic_cast<ImpuStore::AssociatedImpu*>(got_impu);
+
+  ASSERT_NE(nullptr, got_associated_impu);
+  ASSERT_EQ(IMPU, got_associated_impu->default_impu);
+
+  delete got_impu;
+
+  delete impu_store;
+  delete local_store;
+}
+
+TEST_F(ImpuStoreTest, ImpuFromDataEmpty)
+{
+  std::string data;
+  ASSERT_EQ(nullptr, ImpuStore::Impu::from_data(IMPU, data, 0));
+}
+
+TEST_F(ImpuStoreTest, ImpuFromDataIncorrectVersion)
+{
+  std::string data;
+  data.push_back((char) -1);
+  ASSERT_EQ(nullptr, ImpuStore::Impu::from_data(IMPU, data, 0));
+}
+
+TEST_F(ImpuStoreTest, ImpuFromDataVersion0NoLengthOrData)
+{
+  std::string data;
+  data.push_back((char) 0);
+  ASSERT_EQ(nullptr, ImpuStore::Impu::from_data(IMPU, data, 0));
+}
+
+TEST_F(ImpuStoreTest, ImpuFromDataVersion0TooLong)
+{
+  std::string data;
+  data.push_back((char) 0);
+  encode_varbyte(INT_MAX + 1L, data);
+  ASSERT_EQ(nullptr, ImpuStore::Impu::from_data(IMPU, data, 0));
+}
+
+TEST_F(ImpuStoreTest, ImpuFromDataVersion0RunOffEnd)
+{
+  std::string data;
+  data.push_back((char) 0);
+  data.push_back((char) 0x80);
+  ASSERT_EQ(nullptr, ImpuStore::Impu::from_data(IMPU, data, 0));
+}
+
+TEST_F(ImpuStoreTest, ImpuFromDataVersion0InvalidCompressData)
+{
+  std::string data;
+  data.push_back((char) 0);
+  data.push_back((char) 0x1);
+  data.push_back((char) 0xFF);
+  ASSERT_EQ(nullptr, ImpuStore::Impu::from_data(IMPU, data, 0));
 }
