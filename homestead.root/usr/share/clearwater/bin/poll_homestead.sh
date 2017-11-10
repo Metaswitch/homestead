1e88e766b6772cde1edd1c80a71c3851a41f6433
TJW2 <thomas.watson@metaswitch.com>
2017-11-10 16:14:45 +0000

    Improve comments

--- homestead.root/usr/share/clearwater/bin/poll_homestead.sh
+++ homestead.root/usr/share/clearwater/bin/poll_homestead.sh
@@ -9,15 +9,21 @@
 # Otherwise no rights are granted except for those provided to you by
 # Metaswitch Networks in a separate written agreement.
 
-# If the process has not been running for at least the grace period, the script
-# returns zero. This is to allow the process some time to initialize.
-GRACE_PERIOD=20
-
-
 . /etc/clearwater/config
+
+# Set the length of the grace period. If the process has not been running for at
+# least the grace period, the script returns zero. This is to allow the process
+# some time to initialize.
+
+GRACE_PERIOD = 20
+
+# Determine the HTTP IP, poll it, and handle the result according to whether or
+# not we are in the grace period.
+
 http_ip=$(/usr/share/clearwater/bin/bracket-ipv6-address $local_ip)
 /usr/share/clearwater/bin/poll-http $http_ip:8888
 rc=$?
+
 if /usr/share/clearwater/bin/is-stable /var/run/homestead/homestead.pid $GRACE_PERIOD; then
   exit $rc
 else
